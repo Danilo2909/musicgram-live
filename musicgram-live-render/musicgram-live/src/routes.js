bbfa78a5c4e4aac432f7b777003a33f5eac31d19
Danilo2909 <mensslaoq8@gmail.com>
2025-08-28 03:00:25 -0300

    Add files via upload

--- musicgram-live-render/musicgram-live/src/routes.js
+++ musicgram-live-render/musicgram-live/src/routes.js
@@ -1,22 +1,29 @@
-const express = require('express');
+import express from 'express';
+import * as elance from './controllers/elance.js';
+
 const router = express.Router();
 
-const elance = require('./controllers/elance');
-
-const requireAuth = (req,res,next)=>{
-  if(req?.session?.user?.id) return next();
+const requireAuth = (req, res, next) => {
+  if (req?.session?.user?.id) return next();
   return res.redirect('/login');
 };
 
-router.get('/',        requireAuth, elance.home);
-router.get('/discover',requireAuth, elance.discover);
+// HOME (feed)
+router.get('/', requireAuth, elance.home);
+
+// DISCOVER
+router.get('/discover', requireAuth, elance.discover);
+
+// NETWORK
 router.get('/network', requireAuth, elance.network);
 
-router.post('/u/:id/follow',   requireAuth, elance.follow);
+// FOLLOW / UNFOLLOW
+router.post('/u/:id/follow', requireAuth, elance.follow);
 router.post('/u/:id/unfollow', requireAuth, elance.unfollow);
 
-router.post('/p/:id/like',     requireAuth, elance.like);
-router.post('/p/:id/unlike',   requireAuth, elance.unlike);
-router.post('/p/:id/comment',  requireAuth, elance.comment);
+// LIKES & COMMENTS
+router.post('/p/:id/like', requireAuth, elance.like);
+router.post('/p/:id/unlike', requireAuth, elance.unlike);
+router.post('/p/:id/comment', requireAuth, elance.comment);
 
-module.exports = router;
+export default router;
